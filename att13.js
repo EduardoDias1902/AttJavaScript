f4ef99b00dc620acb5dd8ab8492a9d3ad944abbf
eduardo_d_maia <eduardo_d_maia@estudante.sesisenai.org.br>
2025-09-11 14:16:26 -0300

    Att13 finalizada

---
+++ att13.js
@@ -0,0 +1,19 @@
+var um="*";
+var dois="**";
+var tres="***";
+var quatro="****";
+var cinco="*****";
+
+console.log(um);
+console.log(dois);
+console.log(tres);
+console.log(quatro);
+console.log(cinco);
+console.log(cinco+um);
+console.log(cinco+dois);
+
+
+
+
+
+
